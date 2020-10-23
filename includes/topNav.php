e8a126d4ba347667f81a0b0686d1c4faab640d10
saadgfx97 <saadgfx97@gmail.com>
2020-10-23 06:15:37 -0700

    menu final

--- includes/topNav.php
+++ includes/topNav.php
@@ -1,33 +1,29 @@
 <nav class="navbar navbar-light navbar-expand-lg sticky-top pulse animated navigation-clean">
-    <div class="container"><a class="navbar-brand" href="#" style="background-image: url(&quot;assets/img/logo.png&quot;);width: 132px;background-position: center;background-size: cover;">&nbsp;</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+    <div class="container d-flex align-items-center">
+        <a class="navbar-brand" href="#" style="background-image: url(&quot;assets/img/logo.png&quot;);width: 132px;background-position: center;background-size: cover;">&nbsp;</a>
+        <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navcol-1">
             
             
-            <ul class="nav navbar-nav mx-auto" id="navbar-hover">
-                <li class="nav-item" role="presentation"><a class="nav-link active" href="index.php" style="padding-right: 10px;padding-left: 10px;">HOME</a></li>
-                <li class="nav-item dropdown pl-2 ml-lg-0"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">PAGES</a>
+            <ul class="nav navbar-nav ml-auto" id="navbar-hover">
+                <li class="nav-item" role="presentation"><a class="nav-link active" href="index.php">HOME</a></li>
+                <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">PAGES</a>
                     <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">SHORTCODES &amp;WIDGETS</a><a class="dropdown-item" role="presentation" href="loan-comperison.php">LOAN COMPARISION</a><a class="dropdown-item" role="presentation" href="how-it-works.php">HOW IT WORKS</a>
                         <a class="dropdown-item" role="presentation" href="timetable.php">TIMETABLE</a><a class="dropdown-item" role="presentation" href="events.php">EVENTS</a><a class="dropdown-item" role="presentation" href="team.php">TEAM</a><a class="dropdown-item" role="presentation" href="coming-soon.php">COMING SOON</a><a class="dropdown-item" role="presentation" href="#">FAQS</a><a class="dropdown-item" role="presentation" href="404.php">404</a>
                     </div>
                 </li>
-                <li class="nav-item dropdown pl-2 ml-lg-0"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="services.php">SERVICES</a>
+                <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="services.php">SERVICES</a>
                     <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="services.php">SERVICES</a><a class="dropdown-item" role="presentation" href="#">SINGLE SERVICES</a></div>
                 </li>
-                <li class="nav-item dropdown pl-2 ml-lg-0"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">BLOG</a>
+                <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">BLOG</a>
                     <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="blog-grid.php">GRID BLOG</a><a class="dropdown-item" role="presentation" href="blog-post.php">POST</a></div>
                 </li>
-                <li class="nav-item" role="presentation"><a class="nav-link" href="contact.php" style="padding-right: 10px;padding-left: 10px;">CONTACT</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="contact.php">CONTACT</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="#">REGISTER</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="#">LOGIN</a></li>
             </ul>
             
-            <ul class="nav navbar-nav" id="d-none">
-                <li class="nav-item" role="presentation">
-                    <a class="nav-link active d-lg-flex justify-content-lg-end align-items-lg-end mt-5 mt-lg-0" href="#" style="padding-left: 0px;">
-                        <ul class="list-unstyled fa-ul">
-                            <li style="cursor: text;font-family: Montserrat, sans-serif;color: rgb(0,0,0);"><i class="fa fa-phone fa-li" style="color: #3aadaa;"></i>&nbsp;<span style="font-weight: 700;font-family: 'Montserrat';">1800</span>-333-777</li>
-                        </ul>
-                    </a>
-                </li>
-            </ul>
+    
             
         </div>
     </div>
