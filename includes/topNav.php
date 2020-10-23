b1ca498b7a7d16d0fe4565d0c8a34862c8ac1746
saadgfx97 <saadgfx97@gmail.com>
2020-10-23 04:04:12 -0700

    changed the nav hover

--- includes/topNav.php
+++ includes/topNav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-light navbar-expand-md sticky-top pulse animated navigation-clean">
+<nav class="navbar navbar-light navbar-expand-lg sticky-top pulse animated navigation-clean">
     <div class="container"><a class="navbar-brand" href="#" style="background-image: url(&quot;assets/img/logo.png&quot;);width: 132px;background-position: center;background-size: cover;">&nbsp;</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navcol-1">
             
@@ -16,10 +16,20 @@
                 <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">BLOG</a>
                     <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="blog-grid.php">GRID BLOG</a><a class="dropdown-item" role="presentation" href="blog-post.php">POST</a></div>
                 </li>
-                <li class="nav-item" role="presentation"><a class="nav-link active" href="contact.php" style="padding-right: 10px;padding-left: 10px;">CONTACT</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="contact.php" style="padding-right: 10px;padding-left: 10px;">CONTACT</a></li>
             </ul>
             
+            <ul class="nav navbar-nav" id="d-none">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link active text-center d-lg-flex justify-content-lg-end align-items-lg-end" href="#" style="padding-left: 0px;">
+                        <ul class="list-unstyled fa-ul">
+                            <li style="cursor: text;font-family: Montserrat, sans-serif;color: rgb(0,0,0);"><i class="fa fa-phone fa-li" style="color: #3aadaa;"></i>&nbsp;<span style="font-weight: 700;font-family: 'Montserrat';">1800</span>-333-777</li>
+                        </ul>
+                    </a>
+                </li>
+            </ul>
             
+<!--
             <ul class="nav navbar-nav ml-auto" id="d-none" style="margin-left: 0px;">
                 <li class="nav-item" role="presentation">
                     <a class="nav-link active text-center d-lg-flex justify-content-lg-end align-items-lg-end" href="#" style="padding-left: 0px;">
@@ -29,6 +39,7 @@
                     </a>
                 </li>
             </ul>
+-->
         </div>
     </div>
 </nav>
